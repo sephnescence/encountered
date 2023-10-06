9e84112fca6aa81906fa8fe00ba8393b3c5f7767
Blake Taylor <sephnescence@gmail.com>
2023-10-06 21:17:54 +1000

    Nova - Show role on user
    Clean up

--- encountered-backend-laravel/app/Nova/Role.php
+++ encountered-backend-laravel/app/Nova/Role.php
@@ -2,24 +2,17 @@
 
 namespace App\Nova;
 
-use Illuminate\Validation\Rules;
-use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\DateTime;
-use Laravel\Nova\Fields\Gravatar;
-use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Password;
-use Laravel\Nova\Fields\PasswordConfirmation;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-class User extends Resource
+class Role extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
-     * @var class-string<\App\Models\User>
+     * @var class-string<\App\Models\Role>
      */
-    public static $model = \App\Models\User::class;
+    public static $model = \App\Models\Role::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
@@ -34,7 +27,7 @@ class User extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'email',
+        'id',
     ];
 
     /**
@@ -46,34 +39,7 @@ class User extends Resource
     public function fields(NovaRequest $request)
     {
         return [
-            ID::make()->sortable()->hideFromIndex(),
-
-            Gravatar::make()->maxWidth(50),
-
-            Text::make('Name')
-                ->sortable()
-                ->rules('required', 'max:255'),
-
-            Text::make('Email')
-                ->sortable()
-                ->rules('required', 'email', 'max:254')
-                ->creationRules('unique:users,email')
-                ->updateRules('unique:users,email,{{resourceId}}'),
-
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', Rules\Password::defaults(), 'confirmed')
-                ->updateRules('nullable', Rules\Password::defaults(), 'confirmed'),
-
-            PasswordConfirmation::make('Password Confirmation'),
-
-            DateTime::make('created_at')->exceptOnForms()->sortable(),
-
-            // The direction of the relationship is important in Nova. If this was a HasOne,
-            //  clicking on the link would actually take you to the edit page of the role,
-            //  not the user. e.g. we want to be able to select between user and admin,
-            //  instead of being taken to a page where we can rename admin to administrator
-            BelongsTo::make('Role'),
+            Text::make('name'),
         ];
     }
 
